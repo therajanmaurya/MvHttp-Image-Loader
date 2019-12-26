6d57b1d813cbfc9f9ea5a49cfbe43937cf163907
Rajan Maurya <rajanmaurya154@gmail.com>
2019-12-27 00:59:15 +0530

    Fix: Show Sweet Error UI

--- app/src/main/java/com/github/imagemindvalley/MainActivity.kt
+++ app/src/main/java/com/github/imagemindvalley/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.github.imagemindvalley
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.github.mvhttpclient.repository.ImageLoader
 import com.github.mvhttpclient.repository.Status
+import com.github.therajanmaurya.sweeterror.SweetUIErrorHandler
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
@@ -18,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var adapter: MainAdapter
     private lateinit var imageLoader: ImageLoader
+    private lateinit var sweetUIErrorHandler: SweetUIErrorHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,28 +29,48 @@ class MainActivity : AppCompatActivity() {
         (application as ImageApplication).dispatchingAndroidInjector.inject(this)
         mainViewModel =
             ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
-        imageLoader = ImageLoader(this,this, mainViewModel.getMvHttpRepository)
+        imageLoader = ImageLoader(this, this, mainViewModel.getMvHttpRepository)
+        sweetUIErrorHandler = SweetUIErrorHandler(this, findViewById(android.R.id.content))
 
         adapter = MainAdapter(imageLoader)
         rvImages.setHasFixedSize(true)
         rvImages.adapter = adapter
 
+        findViewById<Button>(R.id.btnTryAgain).setOnClickListener {
+            showProgressBar(); fetchImages()
+        }
+
+        fetchImages()
+    }
+
+    private fun fetchImages() {
         mainViewModel.getImageData().observe(this, Observer {
             when (it.status) {
                 Status.SUCCESS -> {
+                    pbImages.visibility = View.GONE
                     if (it.data != null && it.data!!.isNotEmpty()) {
+                        sweetUIErrorHandler.hideSweetErrorLayoutUI(rvImages, sweetLayoutError)
                         adapter.submitList(it.data)
                         adapter.notifyDataSetChanged()
                     }
                 }
                 Status.ERROR -> {
-                    // Show Error UI
+                    pbImages.visibility = View.GONE
+                    sweetUIErrorHandler.showSweetErrorUI(
+                        getString(R.string.images), rvImages, sweetLayoutError
+                    )
                 }
                 Status.LOADING -> {
-                    // Show Loading Progress Bar
+                    showProgressBar()
                 }
             }
         })
+    }
+
+    private fun showProgressBar() {
+        pbImages.visibility = View.VISIBLE
+        rvImages.visibility = View.GONE
+        sweetLayoutError.visibility = View.GONE
     }
 
     override fun onDestroy() {
